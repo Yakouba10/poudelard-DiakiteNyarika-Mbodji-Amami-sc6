3bd584df9bf6b86e81e602ed336bf9eed9fb795b
abmbodj07 <alioune.mbodji@efrei.net>
2025-12-08 16:21:00 +0100

    j'ai continué la partie univers maison

--- Univers/maison.py
+++ Univers/maison.py
@@ -13,3 +13,17 @@ def actualiser_points_maison(maisons, nom_maison, points) :
         print(f"{nom_maison} introuvable")
 
 def afficher_maison_gagnante(maisons) :
+    max_points = max(maisons.values())
+    liste_gagnante = []
+    for nom_maison, points in maisons.items() :
+        if points == max_points :
+            liste_gagnante.append(nom_maison)
+    if len(liste_gagnante) == 1 :
+        print(f" liste_gagnante[0] a gagné")
+    else :
+        print("Egalité entre ", *liste_gagnante, sep=" ,")
+
+
+
+
+
