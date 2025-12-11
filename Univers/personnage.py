daa7018a9b74f79dba61d9ab5134d3ef7dd36b05
Yakouba10 <yakouba.diakite@efrei.net>
2025-12-11 17:26:09 +0100

    J'ai modifié la partie univers personnage

--- Univers/personnage.py
+++ Univers/personnage.py
@@ -20,19 +20,15 @@ def afficher_personnage(joueur):
     print("Profil du personnage :")
 
     for cle, valeur in joueur.items():
-        # Cas 1 : dictionnaire (ex : attributs)
         if isinstance(valeur, dict):
             print(f"{cle} :")
             for scle, sval in valeur.items():
                 print(f" - {scle} : {sval}")
 
-        # Cas 2 : liste (inventaire, sortilèges)
         elif isinstance(valeur, list):
-            # jointure en chaîne si la liste n'est pas vide
             contenu = ", ".join(str(x) for x in valeur)
             print(f"{cle} : {contenu}")
 
-        # Cas 3 : valeur simple
         else:
             print(f"{cle} : {valeur}")
 
