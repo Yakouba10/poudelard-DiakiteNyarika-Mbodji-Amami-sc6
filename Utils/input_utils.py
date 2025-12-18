b91487457b4071345debfdcf32fb089cda5af2bf
ines <ines.amami@efrei.net>
2025-12-18 16:07:12 +0100

    Ajout des fonctions utilitaires dans le fichier imput_utils.py

---
+++ Utils/input_utils.py
@@ -0,0 +1,45 @@
+def demander_texte(message):
+    return input(message)
+
+def demander_nombre(message, min_val=None, max_val=None):
+    while True:
+        try:
+            valeur = int(demander_texte(message))
+
+            if min_val is not None and valeur < min_val:
+                print(f"Erreur : Choisir une valeur comprise entre 1 et 10")
+                continue
+
+            if max_val is not None and valeur > max_val:
+                print(f"Erreur : Choisir une valeur comprise entre 1 et 10")
+                continue
+
+            return valeur
+
+        except ValueError:
+            print("Erreur : Choisir un nombre entier.")
+
+
+def demander_choix(message, options):
+    rep= False
+
+    while not rep:
+        print(message)
+        compteur = 1
+        for i in options:
+            print(str(compteur) + "." + str(i))
+            compteur = compteur + 1
+
+        try:
+            choix = int(demander_texte(" "))
+
+            if 1 <= choix <= len(options):
+                rep = True
+                return choix
+            else:
+                print("Mauvaise saisie, veuillez recommencer.\n")
+
+        except ValueError:
+            print("Mauvaise saisie, veuillez recommencer.\n")
+
+
---
+++ menu.py
@@ -0,0 +1,4 @@
+from Chapitre.Chapitre_1 import creer_personnage, recevoir_lettre, rencontrer_hagrid
+from Utils.input_utils import demander_choix
+
+rencontrer_hagrid()
