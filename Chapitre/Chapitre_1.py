c4785ff358285f44965e245759dab40b70b153b3
inesamamipython <ines.amami@efrei.net>
2025-12-21 18:39:56 +0100

    Update Chapitre_1.py
    utilisation des fonctions utilitaires

--- Chapitre/Chapitre_1.py
+++ Chapitre/Chapitre_1.py
@@ -1,25 +1,25 @@
+from Utils.input_utils import demander_texte, demander_nombre, demander_choix
+
+
 def introduction():
     print ("Bonjour et bienvenue dans le jeu. L'histoire commence ... ")
 
 def creer_personnage():
     attributs={}
-    nom = input("Entrez le nom du personnage : ")
-    prenom = input("Entrez le prénom du personnage : ")
-    attributs["note_courage"] = input("Entrez une valeur de 1 à 10 pour évaluer votre courage : ")
-    attributs["note_intelligence"] = input("Entrez une valeur de 1 à 10 pour évaluer votre intelligence : ")
-    attributs["note_loyaute"] = input("Entrez une valeur de 1 à 10 pour évaluer votre loyauté : ")
-    attributs["note_ambition"]= input("Entrez une valeur de 1 à 10 pour évaluer votre ambition : ")
+    nom = demander_texte("Entrez le nom du personnage : ")
+    prenom = demander_texte("Entrez le prénom du personnage : ")
+    attributs["note_courage"] = demander_nombre("Entrez une valeur de 1 à 10 pour évaluer votre courage : ",1,10)
+    attributs["note_intelligence"] =demander_nombre("Entrez une valeur de 1 à 10 pour évaluer votre intelligence : ",1,10)
+    attributs["note_loyaute"] = demander_nombre("Entrez une valeur de 1 à 10 pour évaluer votre loyauté : ",1,10)
+    attributs["note_ambition"]= demander_nombre("Entrez une valeur de 1 à 10 pour évaluer votre ambition : ",1,10)
 
     return(nom, prenom, attributs)
 
 def recevoir_lettre():
-    print("Une chouette traverse la fenêtre et vous apporte une lettre scellée du sceau de Poudlard…"
-          "\n\n« Cher élève nous avons le plaisir de vous informer que vous avez été admis à l’école de sorcellerie de Poudlard ! »"
-            "\n\nSouhaitez-vous accepter cette invitation et partir pour Poudlard ?"+"\n1. Oui, bien sûr !\n2. Non, je préfère rester avec l’oncle Vernon…")
 
-    reponse = input()
-    if reponse != "1" and reponse != "2":
-        exit('Mauvaise saisie. Fin du jeu')
+    reponse = demander_choix("Une chouette traverse la fenêtre et vous apporte une lettre scellée du sceau de Poudlard…"
+          "\n\n« Cher élève nous avons le plaisir de vous informer que vous avez été admis à l’école de sorcellerie de Poudlard ! »"
+            "\n\nSouhaitez-vous accepter cette invitation et partir pour Poudlard ?",["\n1. Oui, bien sûr !","\n2. Non, je préfère rester avec l’oncle Vernon…"])
 
     if reponse == "2":
         print("\n\n Vous déchirez la lettre, l’oncle Vernon pousse un cri de joie: « EXCELLENT ! Enfin quelqu’un de NORMAL dans cette maison ! » "
@@ -27,10 +27,9 @@ def recevoir_lettre():
         exit(0)
 
 def rencontrer_hagrid():
-    print(" Hagrid : Salut. Je suis venu t'aider à faire tes achats sur le chemin de Traverse.\n\n"
-                                                            "Voulez vous suivre Hagrid ?\n\n1. Oui\n\n2. Non")
-    reponse=input()
-    if reponse != "1" and reponse != "2": exit()
+
+    reponse=demander_choix(" Hagrid : Salut. Je suis venu t'aider à faire tes achats sur le chemin de Traverse.\n\n"
+                                                            "Voulez vous suivre Hagrid ?", ["1. Oui","2. Non"])
     if reponse == "2": print("\nFais moi confiance tu dois me suivre, tu ne le regrettera pas !")
 
 
