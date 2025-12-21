08de30dd919f00770c3cace598df90c0cb6ba67a
inesamamipython <ines.amami@efrei.net>
2025-12-21 19:50:32 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from Chapitre.Chapitre_1 import creer_personnage, recevoir_lettre, rencontrer_hagrid
 from Utils.input_utils import demander_choix
+from Chapitre.Chapitre_1 import chapitre1
 
-rencontrer_hagrid()
+chapitre1()
